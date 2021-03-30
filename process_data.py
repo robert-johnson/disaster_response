68a14b2455c19ff32c336bc471844f5190764be9
Robert Johnson <johnson.robert@gmail.com>
2021-03-29 21:06:40 -0600

    Update process_data.py

--- process_data.py
+++ process_data.py
@@ -1,1 +1,26 @@
+# Import packages
+import sys
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
 
+def load_data():
+    """Load and merge messages and categories datasets
+    
+    Args:
+    None
+       
+    Returns:
+    df: dataframe. Dataframe containing merged content of messages and categories datasets.
+    """
+    
+    # Load messages dataset
+    messages = pd.read_csv('data/messages.csv')
+    
+    # Load categories dataset
+    categories = pd.read_csv('data/categories.csv')
+    
+    # Merge datasets
+    df = messages.merge(categories, how = 'left', on = ['id'])
+    
+    return df
